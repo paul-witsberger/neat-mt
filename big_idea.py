Updated neatfast, fixed issue with integration (scaling), general code improvements

big_idea.py
@@ -9,6 +9,7 @@ import time
 import neatfast as neat
 from traj_config import *
 from orbit_util import *
+from copy import copy
 
 
 def eval_traj_neat(genome, config):
@@ -42,7 +43,7 @@ def eval_traj_neat(genome, config):
         y0, yf = make_new_bcs()
 
         # Integrate trajectory
-        y, miss_ind = integrate_func_missed_thrust(thrust_fcn, y0, deepcopy(ti), yf, m_dry, T_max_kN, du, tu, mu, fu,
+        y, miss_ind = integrate_func_missed_thrust(thrust_fcn, y0, copy(ti), yf, m_dry, T_max_kN, du, tu, mu, fu,
                                                    Isp, tol=tol)
         # Check if integration was stopped early
         if len(y.shape) == 0:
@@ -55,7 +56,6 @@ def eval_traj_neat(genome, config):
         # Calculate ratio of initial mass to final mass
         m_ratio = y0[-1] / y[-1, -1]
         f[i] = traj_fit_func(yf_actual, yf_target, y0, m_ratio)
-        blah = 0
 
     # Calculate scalar fitness
     if num_cases > 1:
@@ -171,18 +171,20 @@ def traj_fit_func(y, yf, y0, m_ratio):
     dw = np.abs(fix_angle(w2 - w1, np.pi, -np.pi) / np.pi)
     df = np.abs(fix_angle(f2 - f1, np.pi, -np.pi) / np.pi)
 
-    drp = 0 if drp < 0.001 else drp
-    dra = 0 if dra < 0.001 else dra
-    da = 0 if da < 0.001 else da
-    dw = 0 if dw < 0.005 else dw
-    df = 0 if df < 0.005 else df
+    K = 0
+    drp = 0 if drp < 0.01 else drp + K
+    dra = 0 if dra < 0.01 else dra + K
+    da = 0 if da < 0.01 else da + K
+    dw = 0 if dw < 0.01 else dw + K
+    df = 0 if df < 0.01 else df + K
 
     # Set cost function based on final trajectory type
     if elliptical_final:
         # Elliptic final
         weights = np.array([1, 1, 1, 0.5], np.float) * 20
-        f = np.sum(np.max((np.square(np.array([drp, dra, dw, df]) * weights),
-                           np.abs(   np.array([drp, dra, dw, df]) * weights)), axis=0)) + m_ratio * m_ratio
+        # f = np.sum(np.max((np.square(np.array([drp, dra, dw, df]) * weights),
+        #                    np.abs(   np.array([drp, dra, dw, df]) * weights)), axis=0)) + m_ratio * m_ratio
+        f = np.sum(np.square(np.array([drp, dra, dw, df]) * weights)) + m_ratio * m_ratio
     else:
         # Circular final
         weights = np.array([1, 1, 0, 0], np.float) * 20
@@ -194,6 +196,7 @@ def traj_fit_func(y, yf, y0, m_ratio):
         if rp1 < min_allowed_rp:
             f += rp_penalty_multiplier * (1 - rp1 / min_allowed_rp)
 
+    # Penalize for not leaving initial orbit
     if no_thrust_penalty:
         kepl_scales = [ra2 - ra0, rp2 - rp0]
         dy0 = np.sum(np.abs(np.array([ra1 - ra0, rp1 - rp0]) / kepl_scales))
@@ -248,8 +251,8 @@ def integrate_func_missed_thrust(thrust_fcn, y0, time_interval, yf, m_dry, T_max
     scales = np.array([du, du, du, du / tu, du / tu, du / tu, mu])
 
     # Set up timer
-    t_upper_lim = 1.
-    tstart = time.time()
+    # t_upper_lim = 1.
+    # tstart = time.time()
 
     # Main loop
     for i in range(len(ti)-1):
@@ -274,7 +277,7 @@ def integrate_func_missed_thrust(thrust_fcn, y0, time_interval, yf, m_dry, T_max
         if variable_power:
             param = [float(gm), float(m_dry / mu), float(thrust[0]), float(thrust[1]), float(thrust[2]), power_reference, power_min, power_max, float(thrust_power_coef[0]), float(thrust_power_coef[1]), float(thrust_power_coef[2]), float(thrust_power_coef[3]), float(thrust_power_coef[4]), float(isp_power_coef[0]), float(isp_power_coef[1]), float(isp_power_coef[2]), float(isp_power_coef[3]), float(isp_power_coef[4])]
         else:
-            param = [float(gm), float(Isp), float(m_dry / mu), float(thrust[0]), float(thrust[1]), float(thrust[2])]
+            param = [float(gm), float(Isp / du * tu), float(m_dry / mu), float(thrust[0]), float(thrust[1]), float(thrust[2])]
         # propagate from the current state until the next time step
         traj = tbp.prop(list(y[i] / scales), [ti[i], ti[i+1]], param, state_size, time_size, param_size, tol, step_size, int(variable_power))
         # save full trajectory
@@ -301,13 +304,14 @@ def calculate_missed_thrust_events(ti):
     :param ti:
     :return:
     """
-    #TODO see what happens when the duration of missed thrust events is shortened versus lengthed
 
     # Define Weibull distribution parameters
     lambda_tbe = 0.62394 * missed_thrust_tbe_factor
     k_tbe = 0.86737
     lambda_rd = 2.459 * missed_thrust_rd_factor
     k_rd = 1.144
+    max_discovery_delay_days = 3
+    op_recovery_days = 0.5
     # initialize list of indices that experience missed thrust
     miss_indices = list()
     t = 0.
@@ -317,7 +321,8 @@ def calculate_missed_thrust_events(ti):
         if t < ti[-1]:  # check if the next event happens before the end of the time of flight
             # while True:
                 recovery_duration = np.random.weibull(k_rd) * lambda_rd * day_to_sec  # calculate the recovery duration
-                recovery_duration += (np.random.rand() * 3 * day_to_sec) + (0.5 * day_to_sec)  # account for discovery delay and operational recovery
+                recovery_duration += (np.random.rand() * max_discovery_delay_days * day_to_sec) + \
+                                     (op_recovery_days * day_to_sec)  # account for discovery delay and operational recovery
                 miss_indices.append(np.arange(find_nearest(ti, t), find_nearest(ti, t + recovery_duration)))
                 t += recovery_duration
         if t >= ti[-1]:
@@ -325,9 +330,6 @@ def calculate_missed_thrust_events(ti):
                 return np.hstack(miss_indices).astype(int)
             else:
                 return np.array([]).astype(int)
-
-    # cdf(p) = 1 - exp(-(x / lam)**k)
-    # x = lam * nthroot(-ln(1-p), k)
 
 
 def find_nearest(array, value):

builder.py
@@ -61,12 +61,18 @@ def recreate_traj_from_pkl(fname, neat_net=False, print_mass=False, save_traj=Fa
     thrust_vec_body[miss_ind] = 0.
     thrust_vec_inertial = rotate_thrust(thrust_vec_body, y)
 
+    thrust_mag = np.sqrt(np.sum(np.square(thrust_vec_body), 1))
+    thrust_ind = np.argwhere(thrust_mag > 0).ravel()
+
     # Plot the transfer
     arrow_color = 'chocolate'
     missed_color = 'red'
+    thrust_color = 'green'
     fig, ax = plotTraj2D(full_traj[:, 1:-1], False, False, label='Transfer', start=True, end=True, show_legend=False)
     for mi in miss_ind:
-        ax.plot(full_traj[20*mi:20*mi+21, 1] / au_to_km, full_traj[20*mi:20*mi+21, 2] / au_to_km, c=missed_color, zorder=7)
+        ax.plot(full_traj[20*mi:20*mi+20, 1] / au_to_km, full_traj[20*mi:20*mi+20, 2] / au_to_km, c=missed_color, zorder=7)
+    for thi in thrust_ind:
+        ax.plot(full_traj[20*thi:20*thi+20, 1] / au_to_km, full_traj[20*thi:20*thi+20, 2] / au_to_km, c=thrust_color, zorder=7)
     fig, ax = plotTraj2DStruct(yfinal, False, False, fig_ax=(fig, ax), label='Final', show_legend=False)
     fig, ax = plotTraj2DStruct(yinit, False, False, fig_ax=(fig, ax), label='Initial', show_legend=False)
     q_scale = np.max(np.linalg.norm(thrust_vec_body, axis=1)) * 20
@@ -163,15 +169,11 @@ def load_traj(traj_fname='traj_data.hdf5'):
     return t, x, u
 
 
-def sensitivity(t, x, u):
+def sensitivity(t, x, m, u):
     '''
     Calculate sensitivity matrix
     :return:
     '''
-    # Reshape to a vector
-    m = x[:, -1].ravel().reshape((-1, 1))
-    x = np.hstack((x[:, :2], x[:, 3:5])).ravel()
-    u = u.ravel()
 
     # Number of rows = number of timesteps
     n = len(t)
@@ -182,11 +184,13 @@ def sensitivity(t, x, u):
     # for i in range(4*n):
     #     ddvdx[i] = get_ddvdx([x, m, u, i])
     # Parallel
-    with mp.Pool(os.cpu_count() - 1) as p:
+    with mp.Pool(os.cpu_count() - 3) as p:
         ddvdx = p.map_async(get_ddvdx, zip(repeat(x, 4*n), repeat(m, 4*n), repeat(u, 4*n), range(4*n))).get()
 
-    # Calculate L2 norm (Frobenius/matrix norm) of the matrix and return
-    sensitivity = np.sqrt(np.sum(np.square(ddvdx)))
+    # OLD - Calculate L2 norm (Frobenius/matrix norm) of the matrix and return
+    # Calculate the L2 norm of each state
+    sensitivity = np.sqrt(np.sum(np.square(ddvdx), 1)).reshape((-1, 1))
+    # print('Sensitivity = %f' % sensitivity)
     return sensitivity
 
 
@@ -233,6 +237,7 @@ def evaluate_perturbed_trajectory(x_perturbed, m, i):
     net = neat.nn.FeedForwardNetwork.create(genome, config)
     nc = Neurocontroller.init_from_neat_net(net, scales_in, scales_out)
     thrust_fcn = nc.get_thrust_vec_neat
+    del local_dir, config_path, config, fname, genome, net, nc
 
     # Propagate for number of steps
     y0, yf = x_perturbed[first_point*n_dim*2:(first_point+1)*n_dim*2], make_new_bcs()[-1]
@@ -262,47 +267,68 @@ def evaluate_perturbed_trajectory(x_perturbed, m, i):
     thrust_vec_body = get_thrust_history(ti, y_matrix, yf, m_dry, T_max_kN, thrust_fcn)[:, :n_dim]
     thrust_vec_body[miss_ind] = 0.
     thrust_vec_inertial = rotate_thrust(thrust_vec_body, y_matrix)
-    if not np.mod(i+1, 100):
-        print(i+1)
+    # if not np.mod(i+1, 100):
+    #     print(i+1)
     return thrust_vec_inertial.ravel()
+
+
+def save_traj(t, x, m, u, traj_fname='traj_data_desensitized.hdf5'):
+    x = np.reshape(x, (len(t), -1))
+    x = np.hstack((x, m))
+    with h5py.File(traj_fname, 'w') as f:
+        f.create_dataset('t', data=t)
+        f.create_dataset('x', data=x)
+        f.create_dataset('u', data=u)
 
 
 def desensitize():
     # Load trajectory
     t, x, u = load_traj()
 
+    # Reshape to vectors
+    m = x[:, -1].ravel().reshape((-1, 1))
+    x = np.hstack((x[:, :2], x[:, 3:5])).ravel()
+    u = u.ravel()
+
     # Calculate sensitivity of original trajectory
-    s = sensitivity(t, x, u)
-    print('Original sensitivity: %f' % s)
+    s = sensitivity(t, x, m, u)
+    np.save('traj_data_s.npy', s)
+    # print('Original sensitivity: %f' % s)
 
     # Calculate sensitivity of perturbed trajectories
-    dsdx = np.empty(len(x))
+    dsdx = np.empty((len(x), len(x))) # , n_dim*(len(t)-1)))
     for i in range(len(x)):
         x_step, v_step = 1, 0.001
         step = x_step if np.mod(i, 4) < 2 else v_step
         x_perturbed = x.copy()
         x_perturbed[i] += step
-        s_perturbed = sensitivity(t, x_perturbed, u)
-        dsdx[i] = (s_perturbed - s) / step
+        s_perturbed = sensitivity(t, x_perturbed, m, u)
+        dsdx[i] = ((s_perturbed - s) / step).T
+        print('Finished %i / %i' % (i + 1, len(x)))
+    np.save('traj_data_dsdx.npy', dsdx)
 
     # Calculate update to states
-    delta_x = s / dsdx
+    # delta_x = s / dsdx / len(x)
+    delta_x = np.matmul(np.linalg.inv(dsdx), s)
+    np.save('traj_data_delta_x.npy', delta_x)
 
     # Update states
     x += delta_x
+    np.save('traj_data_x.npy', x)
+
+    # Save new trajectory to file
+    save_traj(t, x, m, u)
 
     # Calculate new sensitivity
-    s = sensitivity(x, u ,t)
-    print('New sensitivity: %f' % s)
+    s = sensitivity(t, x, m, u)
+    # print('New sensitivity: %f' % s)
 
-    return x
-
-# TODO - there is a memory leak issue; after each sensitivity analysis runs, the memory use jumps up
+    return x, delta_x, dsdx
 
 # TODO - see if there could be a benefit from computing a step from minimizing delta v and minimizing s, and then add them together
 # TODO - look into effect of learning rate
 # TODO - see if the above two TODO's could be combined into one; use learning rate as a Pareto coefficient
 
-
+# TODO plot the magnitude of sensitivity for both position and velocity as a color along the trajectory
 if __name__ == '__main__':
     desensitize()

plotter.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib
-import matplotlib.pyplot as plt
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from constants import au_to_km
 
@@ -180,12 +181,12 @@ def plotThrustHistory(t, thrust_vec, T_max_kN, show_plot=False, save_plot=True, 
     thrust_mag = np.linalg.norm(thrust_vec, axis=1)
     throttle = thrust_mag / T_max_kN
     angle = np.rad2deg(np.arctan2(thrust_vec[:, 1], thrust_vec[:, 0]))
-    ax1.plot(t, throttle)
+    ax1.step(t, throttle, where='post')
     ax1.set_title('Throttle')
     ax1.set_xlabel('Time (day)')
     ax1.set_ylabel('Throttle')
     ax1.grid(True)
-    ax2.plot(t, angle)
+    ax2.step(t, angle, where='post')
     ax2.set_title('Thrust Angle')
     ax2.set_xlabel('Time (day)')
     ax2.set_ylabel('Angle (deg)')

traj_config.py
@@ -54,7 +54,7 @@ tf = 3 * year_to_sec
 
 # Define scales for the state vectors to non-dimensionalize later
 input_frame = 'kep'  # 'kep', 'mee', 'car'
-r_scale, v_scale = a_jupiter_km, vp_earth_kms
+r_scale, v_scale = a_earth_km, vp_earth_kms
 if input_frame == 'kep':
     if n_dim == 2:
         scales_in = np.array([r_scale, 1, np.pi, np.pi])
@@ -101,7 +101,7 @@ num_outages = 0
 # Choose to add a penalty for going too close to the central body in the fitness function
 rp_penalty = True
 min_allowed_rp = a_earth_km * 0.90
-rp_penalty_multiplier = 1000
+rp_penalty_multiplier = 5000
 
 # Choose to add a penalty for not thrusting at all (just staying in initial orbit)
 no_thrust_penalty = True
@@ -111,8 +111,8 @@ max_energy = - u_sun_km3s2 / 2 / max(a0_max, af_max) * 0.8
 min_energy = - u_sun_km3s2 / 2 / min(a0_min, af_min) * 1.2
 
 # Choose whether missed thrust events occur or not, and scale time-between-events and recovery-duration
-missed_thrust_allowed = False
-missed_thrust_tbe_factor = 0.1
+missed_thrust_allowed = True
+missed_thrust_tbe_factor = 0.5
 missed_thrust_rd_factor = 2
 
 # Specify the indices of the input array that should be used